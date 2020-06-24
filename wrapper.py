91bd68400ae76f604266017f5e6d969f74c8c572
x0y14 <ya01ma08@gmail.com>
2020-06-24 18:23:11 +0900

    first commit

---
+++ wrapper.py
@@ -0,0 +1,6 @@
+class Wrapper:
+	def __init__(self):
+		self.holo_schedule_url = "https://schedule.hololive.tv/lives/hololive"
+		self.ichikara_schedule_url = "https://api.itsukaralink.jp/v1.2/events.json"
+
+wrapper = Wrapper()
