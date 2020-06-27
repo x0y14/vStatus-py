fc1ba48897d4a21539a98b1a28909b525804d738
x0y14 <ya01ma08@gmail.com>
2020-06-27 15:42:14 +0900

    動かんくなったから直した

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ import json
 from flask import Flask, jsonify
 import tempfile
 import os
+from datetime import datetime, timedelta, timezone
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 wrapper = Wrapper()
+JST = timezone(timedelta(hours=+9), 'JST')
+
 
 if os.path.exists('/tmp/vSchedule.json') == False:
 	try:
-		with open('/tmp/vSchedule.json', 'w') as f:
-			ichikaraSchedule = wrapper.getIchikaraSchedule()
-			ichikaraCommon = wrapper.changeIchikaraFormatToCommon(ichikaraSchedule)
-			hololiveSchedule = wrapper.getHololiveSchedule()
-			hololiveCommon = wrapper.changeHoloduleFormatToCommon(hololiveSchedule)
+		ichikaraSchedule = wrapper.getIchikaraSchedule()
+		ichikaraCommon = wrapper.changeIchikaraFormatToCommon(ichikaraSchedule)
+		hololiveSchedule = wrapper.getHololiveSchedule()
+		hololiveCommon = wrapper.changeHoloduleFormatToCommon(hololiveSchedule)
 
-			vSchedule = wrapper.integrationCommonSchedules(hololiveCommon=hololiveCommon, ichikaraCommon=ichikaraCommon)
-			with open('/tmp/vSchedule.json', 'w', encoding='utf-8') as f:
+		vSchedule = wrapper.integrationCommonSchedules(hololiveCommon=hololiveCommon, ichikaraCommon=ichikaraCommon)
+		vSchedule['lastUpdate'] = datetime.now(JST).isoformat()
+		try:
+			with open('/tmp/vSchedule.json', 'w') as f:
 				json.dump(vSchedule, f, ensure_ascii=False)
+		except Exception as e:
+			print(e)
+
 	except Exception as e:
 		print(e)
 
@@ -38,23 +46,42 @@ def getSchedule():
 	hololiveCommon = wrapper.changeHoloduleFormatToCommon(hololiveSchedule)
 
 	vSchedule = wrapper.integrationCommonSchedules(hololiveCommon=hololiveCommon, ichikaraCommon=ichikaraCommon)
-	# with tempfile.TemporaryDirectory() as temp_path:
-	# write_path = '/tmp' + '/vSchedule.json'
-	with open('/tmp/vSchedule.json', 'w', encoding='utf-8') as f:
-		json.dump(vSchedule, f, ensure_ascii=False)
+	vSchedule['lastUpdate'] = datetime.now(JST).isoformat()
+
+	try:
+		with open('/tmp/vSchedule.json', 'w', encoding='utf-8') as f:
+			json.dump(vSchedule, f, ensure_ascii=False)
+	except Exception as e:
+		print(e)
 	return jsonify({'status': 'ok'}), 200
 
 
 @app.route('/api/schedule/<time>', methods=['GET'])
 def getNowStreaming(time=None):
-	with open('/tmp/vSchedule.json', 'r', encoding='utf-8') as f:
-		vSchedule = json.loads(f.read())
+	try:
+		with open('/tmp/vSchedule.json', 'r', encoding='utf-8') as f:
+		# json_lines = [ json.loads(s) for s in responses if s != "" ]
+			vSchedule = json.loads(f.read())
+	except Exception as e:
+		return jsonify({'err': str(e)}), 500
+
 	if time in ['past', 'now', 'future']:
-		return jsonify({time: vSchedule[time]}), 200
+		return jsonify(vSchedule[time]), 200
 	elif time == 'all':
 		return jsonify(vSchedule), 200
 	else:
 		return jsonify({}), 404
+
+@app.route('/api/lastUpdate', methods=['GET'])
+def getLastUpdate():
+	try:
+		with open('/tmp/vSchedule.json', 'r', encoding='utf-8') as f:
+		# json_lines = [ json.loads(s) for s in responses if s != "" ]
+			vSchedule = json.loads(f.read())
+	except Exception as e:
+		return jsonify({'err': str(e)}), 500
+	
+	return jsonify({'lastUpdate': vSchedule['lastUpdate']}), 200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
